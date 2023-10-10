251f61725db87debb44d144f624da9be40078e43
kaylee2319 <kayleeharding23@gmail.com>
2023-10-10 13:35:09 -0500

    Added the start of the todo app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import HomePage from './components/HomePage';
 import About from './components/About';
 import Contact from './components/ContactMe';
 import Portfolio from './components/Portfolio';
-import Military from './components/Military'
-import Reactapp from './components/React'
-import Figma from './components/Figma'
-import Photoshop from './components/Photoshop'
+import Military from './components/Military';
+import Reactapp from './components/React';
+import Figma from './components/Figma';
+import Photoshop from './components/Photoshop';
+import ToDo from './components/ToDo';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import SignUp from './components/SignUp';
+import ForgotPassword from './components/ForgotPassword';
 
 function App() {
   return (
@@ -21,6 +26,11 @@ function App() {
           <Route exact path="/photoshop" component={Photoshop} />
           <Route exact path="/figma" component={Figma} />
           <Route exact path="/military" component={Military} />
+          <Route exact path="/todo" component={ToDo} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/forgotpassword" component={ForgotPassword} />
         </Switch>
       </BrowserRouter>
   );
---
+++ src/components/ToDo.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NavBar from './NavBar';
+import Footer from './Footer';
+import '../css/ToDo.css';
+
+
+const ToDo = () => {
+
+  return (
+    <body className="ToDoPage">
+    <NavBar />
+      <div className="ToDoPage_InfoBackGround">
+        hello
+      </div>
+      <Footer />
+    </body>
+  );
+};
+export default ToDo
